944be9f46116936238eff55931ef3fc46fc72e71
Rafik Gevorgyan <rafikgevorgyan@Rafiks-MacBook-Pro.local>
2023-11-12 16:42:52 +0400

    style change

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -50,7 +50,7 @@ function Pagination({ currentPage, pageSize, dataSize, onPageChange }: Paginatio
                         'flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700';
                     if (currentPage === num)
                         className =
-                            'flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border  text-white bg-blue-600 border-blue-600 hover:bg-none';
+                            'flex items-center justify-center px-3 h-8 leading-tight text-white bg-blue-600 border-blue-600 hover:bg-none';
                     return (
                         <li
                             key={num}
